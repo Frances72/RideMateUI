2d7672d9ccc2dfbd568e456280c01255a6f302e4
Frances <Francesanderson3@gmail.com>
2015-09-13 12:58:35 +0200

    Third Commit

--- app/src/main/java/fa/tp2/ridemateui/View.java
+++
@@ -1,7 +0,0 @@
-package fa.tp2.ridemateui;
-
-/**
- * Created by User on 2015/09/12.
- */
-public class View {
-}
--- app/src/main/java/fa/tp2/ridemateui/views/SubmitUpdateActivity.java
+++ app/src/main/java/fa/tp2/ridemateui/views/SubmitUpdateActivity.java
@@ -1,7 +1,41 @@
 package fa.tp2.ridemateui.views;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import fa.tp2.ridemateui.R;
+
 /**
  * Created by User on 2015/09/12.
  */
-public class SubmitUpdateActivity {
+public class SubmitUpdateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submit_update);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_submit_update, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
